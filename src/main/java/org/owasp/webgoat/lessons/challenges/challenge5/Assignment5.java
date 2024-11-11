cfa53737298becf0974ed06c25fa9551f65e6d8e
SebasTuPython <sebastian.martinez.dosa@gmail.com>
2024-11-11 17:54:43 +0100

    Update Assignment5.java

--- src/main/java/org/owasp/webgoat/lessons/challenges/challenge5/Assignment5.java
+++ src/main/java/org/owasp/webgoat/lessons/challenges/challenge5/Assignment5.java
@@ -47,27 +47,29 @@ public class Assignment5 extends AssignmentEndpoint {
   @PostMapping("/challenge/5")
   @ResponseBody
   public AttackResult login(
-      @RequestParam String username_login, @RequestParam String password_login) throws Exception {
+      @RequestParam String username_login, 
+      @RequestParam String password_login) throws Exception {
+      
     if (!StringUtils.hasText(username_login) || !StringUtils.hasText(password_login)) {
       return failed(this).feedback("required4").build();
     }
+    
     if (!"Larry".equals(username_login)) {
       return failed(this).feedback("user.not.larry").feedbackArgs(username_login).build();
     }
-    try (var connection = dataSource.getConnection()) {
-      PreparedStatement statement =
-          connection.prepareStatement(
-              "select password from challenge_users where userid = '"
-                  + username_login
-                  + "' and password = '"
-                  + password_login
-                  + "'");
-      ResultSet resultSet = statement.executeQuery();
 
-      if (resultSet.next()) {
-        return success(this).feedback("challenge.solved").feedbackArgs(flags.getFlag(5)).build();
-      } else {
-        return failed(this).feedback("challenge.close").build();
+    try (var connection = dataSource.getConnection()) {
+      String query = "SELECT password FROM challenge_users WHERE userid = ? AND password = ?";
+      try (PreparedStatement statement = connection.prepareStatement(query)) {
+        statement.setString(1, username_login);
+        statement.setString(2, password_login);
+
+        ResultSet resultSet = statement.executeQuery();
+        if (resultSet.next()) {
+          return success(this).feedback("challenge.solved").feedbackArgs(flags.getFlag(5)).build();
+        } else {
+          return failed(this).feedback("challenge.close").build();
+        }
       }
     }
   }
